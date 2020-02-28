a123abaa6ceba1d9bc69ade22f06c4cb5dfeb99c
Christian Dupuis <cd@atomist.com>
2020-02-28 17:29:08 +0100

    Update to action filter in code

--- events/ConvergePullRequestAutoRebaseLabels.ts
+++ events/ConvergePullRequestAutoRebaseLabels.ts
@@ -21,18 +21,39 @@ import {
     apiUrl,
     gitHub,
 } from "./github";
-import { ConvergePullRequestAutoRebaseLabelsSubscription } from "./types";
+import {
+    ConvergePullRequestAutoRebaseLabelsSubscription,
+    PullRequestAction,
+} from "./types";
 
 export const AutoRebaseOnPushLabel = "auto-rebase:on-push";
 
 export const handler: EventHandler<ConvergePullRequestAutoRebaseLabelsSubscription> = async ctx => {
+    const pr = ctx.data.PullRequest[0];
+
+    if (pr.action !== PullRequestAction.Opened) {
+        await ctx.audit.log(`Pull request ${pr.repo.owner}/${pr.repo.name}#${pr.number} not opened. Ignoring...`);
+
+        return {
+            code: 0,
+            reason: `Pull request [${pr.repo.owner}/${pr.repo.name}#${pr.number}](${pr.url}) not opened. Ignoring...`,
+        };
+    }
+
     const repo = ctx.data.PullRequest[0].repo;
     const { owner, name } = repo;
     const credentials = await ctx.credential.resolve(gitHubAppToken({ owner, repo: name }));
 
     const api = gitHub(credentials.token, apiUrl(repo));
 
+    await ctx.audit.log(`Converging auto-rebase label`);
+
     await addLabel(AutoRebaseOnPushLabel, "0E8A16", owner, name, api);
+
+    return {
+        code: 0,
+        reason: `Converged auto-rebase label for repository ${repo.owner}/${repo.name}`,
+    };
 };
 
 async function addLabel(name: string,
